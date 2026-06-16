b385d0dd6577d4bec0376043ec05c0902ad1fd99
gade-6900 <gadepatil438@gmail.com>
2026-06-16 22:06:15 +0530

    List Using Leanear search

---
+++ DSA/ListElementSearch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class ListElementSearch {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9.10);
+        int key = 6;
+        System.out.println("Linear search: "+ list.contains(key));
+
+        int index = Collections.binarySearch(list,key);
+        if(index>=0){
+            System.out.println("Element found :"+ index);
+        }
+        else{
+            System.out.println("Element not found: ");
+        }
+    }
+}
