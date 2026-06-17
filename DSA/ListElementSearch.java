6e74ddfa8760b39c40095fb0361207692105eac2
gade-6900 <gadepatil438@gmail.com>
2026-06-17 08:15:37 +0530

    MergeAlterString Leetcode problem

--- DSA/ListElementSearch.java
+++ DSA/ListElementSearch.java
@@ -1,9 +1,8 @@
 import java.util.*;
-
-
+import java.util.List;
 public class ListElementSearch {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9.10);
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         int key = 6;
         System.out.println("Linear search: "+ list.contains(key));
 
---
+++ DSA/MergeAlternateString.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MergeAlternateString {
+    public String MergerAlter(String s1, String s2){
+        StringBuilder SB = new StringBuilder();
+        int i=0;
+        int j=0;
+        while (i<s1.length() && j<s2.length()) {
+            SB.append(s1.charAt(i++));
+            SB.append(s2.charAt(j++));
+        }
+        while(i<s1.length()){
+            SB.append(s1.charAt(i++));
+        }
+        while(j<s2.length()){
+            SB.append(s2.charAt(j++));
+        }
+        return SB.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter String 1: ");
+        String s1 = sc.next();
+
+        System.out.print("Enter Second String: ");
+        String s2 = sc.next();
+
+        MergeAlternateString obj = new MergeAlternateString();
+        String result = obj.MergerAlter(s1, s2);
+            System.out.println(result);
+    }
+}
