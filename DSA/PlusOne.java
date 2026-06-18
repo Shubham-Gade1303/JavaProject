f2c516c32a33a8691f06d37f40faa851efc60b7b
gade-6900 <gadepatil438@gmail.com>
2026-06-18 08:10:58 +0530

    in the plus one program i made mistake where in the conditoin |if(digit[i]>0)| so that sometest cases are not passed but solve this this condition is (if(digits[i]<9)) now it is perfect

--- DSA/PlusOne.java
+++ DSA/PlusOne.java
@@ -5,7 +5,7 @@ public class PlusOne{
 public int[] newAdd(int[] arr){
     for(int i= arr.length-1;i>=0;i--){
 
-        if(arr[i]>0){
+        if(arr[i]<9){
             arr[i]++;
             return arr;
         }
