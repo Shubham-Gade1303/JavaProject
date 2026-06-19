9a013e86df8d6ace5f7bc253e386b0d6442fcb48
gade-6900 <gadepatil438@gmail.com>
2026-06-19 14:01:48 +0530

    Inbuilt Arrays.sort

---
+++ DSA/SortUsingInBuilt.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class SortUsingInBuilt {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,8,6,9,10,15,12,11,14,13};
+
+        Arrays.sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+}
