244d36e766a1bf4fe842c6b58ce12220c713896d
gade-6900 <gadepatil438@gmail.com>
2026-06-19 15:26:25 +0530

    class level

--- DSA/ToCheckSortedArray.java
+++ DSA/ToCheckSortedArray.java
@@ -1,19 +1,30 @@
+import java.util.Scanner;
+
 public class ToCheckSortedArray {
     
     public static void main(String[] args) {
-        int[] arr = {10,40,30,20,50};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements: ");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
 
-        boolean isSort =false;
+        boolean isSort =true;
 
         for(int i=0;i<arr.length-1;i++){
             if(arr[i]>arr[i+1]){
             isSort=false;
             break;
             }
-            System.out.println("Array is not sorted");
+            
         }
-        if(isSort){
-            System.out.println("Array is sorted..");
+        if(!isSort){
+            System.out.println("Array is Not sorted..");
+        }else{
+            System.out.println("Array is sorted...........");
         }
     }
 }
