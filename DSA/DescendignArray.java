696b45454f24e9c954bfbe4039d5e0eae986b80d
gade-6900 <gadepatil438@gmail.com>
2026-06-19 14:52:13 +0530

    coding june

---
+++ DSA/DescendignArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+//import java.util.Collection;
+import java.util.Collections;
+
+public class DescendignArray {
+    public static void main(String[] args) {
+        int[] arr = {100,30,20,10,40,60,70,80,50,90};
+
+        Arrays.sort(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+}
