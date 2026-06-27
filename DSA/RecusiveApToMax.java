279895d4ba1192a8f817d4d6bf2de06ef22ddc3f
gade-6900 <gadepatil438@gmail.com>
2026-06-27 12:55:41 +0530

    completed code

---
+++ DSA/RecusiveApToMax.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class RecusiveApToMax{
+    static int findMax(int[] arr, int i ){
+        if(i == arr.length-1){
+            return arr[i];
+        }
+
+        int recMax = findMax(arr, i+1);
+
+        return Math.max(recMax, arr[i]);
+    }
+    static int large(int[] arr){
+        return findMax(arr,0);
+    }
+    public static void main(String[] args){
+        int[] arr = {20,202,400,500,101};
+
+        System.out.print(large(arr));
+    }
+}
