916a6cb6ca556bbe7eca7e68380ed1a00ea140f2
gade-6900 <gadepatil438@gmail.com>
2026-06-27 13:48:58 +0530

    Arrays.sort used

--- DSA/RecusiveApToMax.java
+++ DSA/RecusiveApToMax.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class RecusiveApToMax{
-    static int findMax(int[] arr, int i ){
-        if(i == arr.length-1){
-            return arr[i];
-        }
+    // static int findMax(int[] arr, int i ){
+    //     if(i == arr.length-1){
+    //         return arr[i];
+    //     }
 
-        int recMax = findMax(arr, i+1);
+    //     int recMax = findMax(arr, i+1);
 
-        return Math.max(recMax, arr[i]);
-    }
+    //     return Math.max(recMax, arr[i]);
+    // }
+    // static int large(int[] arr){
+    //     return findMax(arr,0);
+    // }
+
     static int large(int[] arr){
-        return findMax(arr,0);
+        Arrays.sort(arr);
+        return arr[arr.length-1];
     }
     public static void main(String[] args){
         int[] arr = {20,202,400,500,101};
