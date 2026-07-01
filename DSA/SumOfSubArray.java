3bc86aa2c9de70891d6b4b153e82e2f50025c293
gade-6900 <gadepatil438@gmail.com>
2026-07-01 10:41:57 +0530

    element contribution method used for O(n)time and O(1)space complexity

--- DSA/SumOfSubArray.java
+++ DSA/SumOfSubArray.java
@@ -2,15 +2,18 @@ public class SumOfSubArray {
     
     static int SumOfSubArr(int[] arr){
         int n = arr.length;
-        int result = 0 , temp =0;
+        int result = 0 ;
 
 
+        // for(int i=0;i<n;i++){
+        //     temp =0;
+        //     for(int j =i;j<n;j++){
+        //         temp = temp + arr[j];
+        //         result= result + temp;
+        //     }
         for(int i=0;i<n;i++){
-            temp =0;
-            for(int j =i;j<n;j++){
-                temp = temp + arr[j];
-                result= result + temp;
-            }
+            result += (arr[i]*(i+1)*(n-1));
+        
         }
         return result;
     }
