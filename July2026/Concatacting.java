d588f3b03d190183b7663e3b4050b596b4a1cf22
gade-6900 <gadepatil438@gmail.com>
2026-07-07 21:24:44 +0530

    Frequency count of string first character

---
+++ July2026/Concatacting.java
@@ -0,0 +1,11 @@
+package July2026;
+
+public class Concatacting {
+    public static void main(String[] args) {
+        String s = "java good";
+        String s2 = "programming language";
+
+        String result = s.concat(" " + s2);
+        System.out.println(result);
+    }
+}
