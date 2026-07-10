2810757cf7d4b13e6870cc6fb35f01f7c16111bb
gade-6900 <gadepatil438@gmail.com>
2026-07-10 10:45:47 +0530

    217. Contains Duplicate

---
+++ July2026/CollectionApp/DuplicateContains.java
@@ -0,0 +1,34 @@
+package July2026.CollectionApp;
+
+import java.util.Scanner;
+
+import June_2026.largeNum;
+
+public class DuplicateContains {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int size  = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.print("enter Array: ");
+        for(int i=0;i<size;i++){
+            arr[i] =sc.nextInt();
+        }
+        boolean found = false;
+        for(int i=0;i<arr.length;i++){
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    found = true;
+                    break;
+                }   
+            } 
+        }
+        if(found){
+            System.out.println("Duplicate found...");
+        }
+        else{
+            System.out.println("Not found");
+        }
+       
+    }
+}
